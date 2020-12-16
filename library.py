f0e622875930e5486e6d2e7a2fd9b28de54328cd
Johan Restrepo <johanesteban.restrepo@utp.edu.co>
2020-12-15 22:15:14 -0500

    crop_image function added

--- library.py
+++ library.py
@@ -12,3 +12,24 @@ BLUE_2 = [0,255,255]
 BLACK = [0,0,0]
 WHITE = [255,255,255]
 GRAY = [180,180,180]
+
+def crop_image(image_route, columns, rows):
+    image = pg.image.load  (image_route)
+    image_info = image.get_rect()
+    image_width = image_info[2]
+    image_height = image_info[3]
+
+    sprite_width = image_width // columns
+    sprite_height = image_height // rows
+
+    sprites_row = []
+    sprites_matrix = []
+
+    for row in range(rows):
+        for column in range(columns):
+            sprite = image.subsurface(column*sprite_width, row*sprite_height, sprite_width, sprite_height)
+            sprites_row.append(sprite)
+        sprites_matrix.append(sprites_row)
+        sprites_row = []
+    
+    return sprites_matrix
